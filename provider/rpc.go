6dbb3d1913ea5c31678e45fdce7c8adbf7b04fcc
minhd-vu <minhd_vu@yahoo.com>
2025-03-14 12:16:31 -0400

    fix: handle unsupported txs

--- provider/rpc.go
+++ provider/rpc.go
@@ -17,6 +17,7 @@ import (
 	zkevmtypes "github.com/0xPolygonHermez/zkevm-node/jsonrpc/types"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -441,7 +442,7 @@ func (r *RPCProvider) refreshCheckpoint(ctx context.Context, c *ethclient.Client
 		return
 	}
 
-	inputs := make(map[string]interface{})
+	inputs := make(map[string]any)
 	if err := method.Inputs.UnpackIntoMap(inputs, tx.Data()[4:]); err != nil {
 		r.logger.Error().Err(err).Msg("Failed to unpack input params")
 		return
@@ -510,6 +511,103 @@ func (r *RPCProvider) refreshFinalizedCheckpoint(ctx context.Context, c *ethclie
 	}
 }
 
+type rpcTransaction struct {
+	tx *types.Transaction
+	txExtraInfo
+}
+
+func (tx *rpcTransaction) UnmarshalJSON(msg []byte) error {
+	if err := json.Unmarshal(msg, &tx.tx); err != nil {
+		return err
+	}
+	return json.Unmarshal(msg, &tx.txExtraInfo)
+}
+
+type txExtraInfo struct {
+	BlockNumber *string         `json:"blockNumber,omitempty"`
+	BlockHash   *common.Hash    `json:"blockHash,omitempty"`
+	From        *common.Address `json:"from,omitempty"`
+}
+
+type rpcBlock struct {
+	Hash         common.Hash         `json:"hash"`
+	Transactions []rpcTransaction    `json:"transactions"`
+	UncleHashes  []common.Hash       `json:"uncles"`
+	Withdrawals  []*types.Withdrawal `json:"withdrawals,omitempty"`
+}
+
+// getBlockByNumber gets the block given a block number. This is lifted from
+// https://github.com/ethereum/go-ethereum/blob/master/ethclient/ethclient.go
+// with the one change that all transactions are treated as legacy transactions.
+// This allows panoptichain to observe chains that use transaction types that
+// are not supported by geth.
+func (r *RPCProvider) getBlockByNumber(ctx context.Context, n *big.Int, c *ethclient.Client) (*types.Block, error) {
+	var raw json.RawMessage
+	err := c.Client().Call(&raw, "eth_getBlockByNumber", hexutil.EncodeBig(n), true)
+	if err != nil {
+		return nil, err
+	}
+
+	var head *types.Header
+	if err := json.Unmarshal(raw, &head); err != nil {
+		return nil, err
+	}
+
+	var body map[string]any
+	if err := json.Unmarshal(raw, &body); err != nil {
+		return nil, err
+	}
+
+	transactions := body["transactions"].([]any)
+	for _, tx := range transactions {
+		tx := tx.(map[string]any)
+		// Remove the transaction type field which would allow the transaction to be
+		// treated as legacy.
+		delete(tx, "type")
+	}
+
+	bytes, err := json.Marshal(body)
+
+	var block rpcBlock
+	if err := json.Unmarshal(bytes, &block); err != nil {
+		return nil, err
+	}
+
+	var uncles []*types.Header
+	if len(block.UncleHashes) > 0 {
+		uncles = make([]*types.Header, len(block.UncleHashes))
+		reqs := make([]rpc.BatchElem, len(block.UncleHashes))
+
+		for i := range reqs {
+			reqs[i] = rpc.BatchElem{
+				Method: "eth_getUncleByBlockHashAndIndex",
+				Args:   []any{block.Hash, hexutil.EncodeUint64(uint64(i))},
+				Result: &uncles[i],
+			}
+		}
+
+		if err := c.Client().BatchCallContext(ctx, reqs); err != nil {
+			return nil, err
+		}
+
+		for i := range reqs {
+			if reqs[i].Error != nil {
+				return nil, reqs[i].Error
+			}
+			if uncles[i] == nil {
+				return nil, fmt.Errorf("got null header for uncle %d of block %x", i, block.Hash[:])
+			}
+		}
+	}
+
+	txs := make([]*types.Transaction, len(block.Transactions))
+	for i, tx := range block.Transactions {
+		txs[i] = tx.tx
+	}
+
+	return types.NewBlockWithHeader(head).WithBody(txs, uncles).WithWithdrawals(block.Withdrawals), nil
+}
+
 // fillRange will pull all of the blocks between the start and the current head.
 func (r *RPCProvider) fillRange(ctx context.Context, start uint64, c *ethclient.Client) {
 	r.logger.Debug().
@@ -519,12 +617,19 @@ func (r *RPCProvider) fillRange(ctx context.Context, start uint64, c *ethclient.
 		Msg("Filling block range")
 
 	for i := start + 1; i <= r.BlockNumber; i++ {
-		b, err := c.BlockByNumber(ctx, new(big.Int).SetUint64(i))
+		num := new(big.Int).SetUint64(i)
+		block, err := c.BlockByNumber(ctx, num)
+
+		// Retry fetching the block if it's an unrecognized transaction.
+		if errors.Is(err, types.ErrTxTypeNotSupported) {
+			block, err = r.getBlockByNumber(ctx, num, c)
+		}
+
 		if err != nil {
 			r.logger.Warn().Err(err).Uint64("block_number", i).Msg("Unable to get block")
-			break
 		}
-		r.blockBuffer.PutBlock(b)
+
+		r.blockBuffer.PutBlock(block)
 	}
 }
 
@@ -556,7 +661,7 @@ func (r *RPCProvider) refreshWalletBalance(ctx context.Context, c *ethclient.Cli
 		r := new(json.RawMessage)
 		reqs[index] = rpc.BatchElem{
 			Method: "eth_getBalance",
-			Args:   []interface{}{addr, "latest"},
+			Args:   []any{addr, "latest"},
 			Result: r,
 			Error:  nil,
 		}
